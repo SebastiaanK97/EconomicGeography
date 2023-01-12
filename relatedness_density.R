c86af429da626d420a64362ba1369b4aec2624bc
SebastiaanK97 <71498038+SebastiaanK97@users.noreply.github.com>
2023-01-12 15:36:44 +0100

    Update files

---
+++ relatedness_density.R
@@ -0,0 +1,22 @@
+
+library(tidyverse)
+# http://econ.geo.uu.nl/peeg/peeg1709.pdf
+library(EconGeo)
+# http://stavrakoudis.econ.uoi.gr/r-eurostat/drawing-maps-of-europe.html
+library(eurostat)
+
+library(ggpubr)
+library(ggsci)
+library(scales)
+
+# ---- patent data  ----
+
+setwd("../Data/Patents/OECD_REGPAT_202208/")
+dir()
+
+IPC <- read.csv("202208_EPO_IPC.txt", sep = "|")
+APP <- read.csv("202208_EPO_App_reg.txt", sep = "|")
+INV <- read.csv("202208_EPO_Inv_reg.txt", sep = "|")
+
+
+
