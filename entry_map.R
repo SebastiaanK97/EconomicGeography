d7c0b9dd87223d85ede40b07cd60577abce55158
SebastiaanK97 <71498038+SebastiaanK97@users.noreply.github.com>
2023-01-18 16:50:50 +0100

    Update files

--- entry_map.R
+++ entry_map.R
@@ -60,13 +60,9 @@ geo_data <- geo_data %>%
 geo_data[["geo"]][geo_data[["geo"]] == "IS00"] <- "IS01"
 length(unique(geo_data$geo))
 
-# ---- entry to map ----
+# ---- entry to map periodical ----
 
-technological_entry_period <- read.csv("technological_entry_period.csv", sep=",")
-
-ggplot(EU_EFTA, aes(x=n)) + geom_histogram(fill="#003764", colour="white", bins=25) +
-  theme_pubr(base_size = 18, base_family = "Georgia") +
-  xlab("\nEntry of Technological Fields per Region") + ylab("Density Distribution\n")
+technological_entry_period <- read.csv("Data/technological_entry_period.csv", sep=",")
 
 sum_entry <- technological_entry_period %>%
   group_by(region, period) %>%
@@ -77,6 +73,10 @@ EU_EFTA <- geo_data %>%
   filter(geo %in% unique(technological_entry_period$region)) %>%
   left_join(sum_entry) %>%
   mutate(class=cut(n, classIntervals(sum_entry$n, n=5, style="jenks")$brks, include.lowest=T))
+
+ggplot(EU_EFTA, aes(x=n)) + geom_histogram(fill="#003764", colour="white", bins=25) +
+  theme_pubr(base_size = 18, base_family = "Georgia") +
+  xlab("\nEntry of Technological Fields per Region") + ylab("Density Distribution\n")
 
 map1 <- EU_EFTA %>%
   filter(period == 2) %>%
@@ -111,4 +111,24 @@ map4 <- EU_EFTA %>%
   labs(subtitle="2014-2018")
 
 ggarrange(map1, map2, map3, map4, ncol=2, nrow=2, common.legend=TRUE, legend="bottom")
+
+# ---- entry to map total ----
+
+sum_entry <- technological_entry_period %>%
+  group_by(region) %>%
+  summarise(n=sum(entry, na.rm=TRUE)) %>%
+  select(geo=region, n)
+
+EU_EFTA <- geo_data %>%
+  filter(geo %in% unique(technological_entry_period$region)) %>%
+  left_join(sum_entry) %>%
+  mutate(class=cut(n, classIntervals(sum_entry$n, n=5, style="jenks")$brks, include.lowest=T))
+
+ggplot(EU_EFTA, aes(fill=class)) + geom_sf(size=0.1, colour="#4D4D4D") + 
+  theme_void(base_size = 18, base_family = "Georgia") + theme(legend.position = "none") +
+  scale_x_continuous(limits=c(-25, 35)) + scale_y_continuous(limits=c(35, 70)) +
+  scale_fill_manual(values=c("#E8B63C", "#93BA5A", "#27A581", "#1A7789", "#003764"), name="Entries")
+
+
+
 
